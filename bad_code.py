fb24729831fbc9626261532d04aa5ca26ece45c2
Michael Spitz <mspitz2005@gmail.com>
2025-02-19 19:21:34 -0500

    Final perfect bug free code

--- bad_code.py
+++ bad_code.py
@@ -1,23 +1,28 @@
-from functools import total_ordering
-
-
 class Calculator:
     """
     The version of the calculator that doesn't follow recommended programming practices.
     """
 
     starting_value = 0
+    operations = {}
 
     # The constructor initializes starting value!
     def __init__(self, starting_value):
         self.starting_value = 0
+        print("welcome to the calculator")
+        self.operation = {
+        "1": ("+", self.add),
+        "2": ("-", self.subtract),
+        "3": ("*", self.multiply),
+        "4": ("/", self.divide)
+    }
 
     """ This method declares a variable.
         Then this method ensures addition is required and stores the sum of addition.
         Then the method returns the sum of addition stored in the variable.
     """
-    def add(x, y, flag):
-        total = starting_value
+    def add(self, x, y, flag):
+        total = self.starting_value
         if flag:
             total = x + y
         else:
@@ -28,8 +33,8 @@ class Calculator:
         Then this method ensures addition is required and stores the difference of variables.
         Then the method returns the difference stored in the variable.
     """
-    def subtract(x, y, flag):
-        total = starting_value
+    def subtract(self, x, y, flag):
+        total = self.starting_value
         if flag:
             total = x + y
         else:
@@ -41,18 +46,24 @@ class Calculator:
     # possible implementation of
     # multiplicat -
     # ion
-    def multiply(x, y):
-        total = starting_value
-        for i in range(x):
-            total += y
-        return total
+    def multiply(self, x, y, flag):
+        if flag:
+            total = self.starting_value
+            for i in range(int(x)):
+                total += y
+            return total
+        else:
+            print("Multiplication IMPOSSIBLE!.")
 
     """ This method does something. """
-    def divide(x, y):
-        if y == 0:
-            print("\nCannot divide by zero")
-            return None
-        return x / y
+    def divide(self, x, y, flag):
+        if flag:
+            if y == 0:
+                print("\nCannot divide by zero")
+                return None
+            return x / y
+        else:
+            print("Division IMPOSSIBLE.")
 
     """Find a number the user wants, flag to make sure they want it."""
     def pick_nums(flag):
@@ -66,5 +77,29 @@ class Calculator:
 
 # Test run of the calculator
 calculator = Calculator(99999999)
-print("welcome to the calculator")
+
+while True:
+    print("\nSelect operation:")
+    print("\n\t1.Add")
+    print("\t2.Subtract")
+    print("\t3.Multiply")
+    print("\t4.Divide")
+    print("\t5.Quit")
+
+    choice = input("\nEnter choice(1/2/3/4/5): ")
+
+    if choice == "5":
+        print("\nQuitting...")
+        break
+
+    elif choice in calculator.operation:
+        num1, num2 = calculator.pick_nums()
+        character, function = calculator.operation[choice]
+        result = function(num1, num2, True)
+
+        if result is not None:
+            print(f"\n{num1} {character} {num2} = {result}")
+        else:
+            print("Invalid input, try again.")
+
 
